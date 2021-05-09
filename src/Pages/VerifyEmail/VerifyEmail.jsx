e68b694df65a69f0afecfb5d75fef682792cc417
Aritam <aritamasthana@gmail.com>
2021-05-09 19:51:22 +0530

    snackbar

--- src/Pages/VerifyEmail/VerifyEmail.jsx
+++ src/Pages/VerifyEmail/VerifyEmail.jsx
@@ -1,19 +1,51 @@
 import React, { useState } from 'react'
 import Cookies from 'universal-cookie/es6';
 import './VerifyEmail.css'
-
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
 
 const cookies = new Cookies(); 
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }));
+
+
+
 const VerifyEmail = () =>{
+
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
     const [otp,setOtp] = useState('');
 
     const onInputChange = (event) =>{
         const target = event.target;
         const value = target.value
         setOtp(value);
-        console.log(value,otp)
     }
+
+    // const handleClick = () => {
+    //     setOpen(true);
+    //   };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+      };
 
     const onOtpSubmit = () =>{
         fetch('http://localhost:3000/verify',{
@@ -25,11 +57,13 @@ const VerifyEmail = () =>{
         })
         .then(res => res.json())
         .then(data => {
+            console.log(data)
             if(data.message==='successful verification'){
                 cookies.set('loggedIn','true');
                 window.location.replace('/covidthing');
             }
             else{
+                setOpen(true);
                 console.log("invalid otp")
             }
             console.log(data);
@@ -42,6 +76,11 @@ const VerifyEmail = () =>{
             <p >We have sent you an otp on your registered email</p>
             <input type="text" name="otp" onChange={onInputChange} className="otp"/>
             <button name="submitOtp" className="otp" onClick={onOtpSubmit}>Submit</button>
+            <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error">
+                  Invalid OTP
+                </Alert>
+            </Snackbar>
         </div>
     );
 }
